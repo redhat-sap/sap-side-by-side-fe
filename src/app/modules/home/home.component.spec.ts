cfd877fe06344ce93d6f09730d12c84d8738f398
Marcos Entenza <makentenza@gmail.com>
2020-12-16 22:41:50 +0000

    Update home.component.spec.ts

--- src/app/modules/home/home.component.spec.ts
+++ src/app/modules/home/home.component.spec.ts
@@ -3,7 +3,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { HomeComponent } from './home.component';
 
 describe('HomeComponent', () => {
-  let component: HomeComponent;
+  let component; HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
 
   beforeEach(async(() => {
